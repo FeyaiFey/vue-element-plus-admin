03eadecd811300ce1b34f988b86356c9b8abcfdd
wxb <wxb1@h-sun.com>
2024-12-18 16:32:18 +0800

    chore: 更新一些静态文件、环境名称和项目名称

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,7 @@ export const constantRouterMap: AppRouteRecordRaw[] = [
     children: [
       {
         path: '/redirect/:path(.*)',
-        name: 'Redirect',
+        name: 'ChildRedirect',
         component: () => import('@/views/Redirect/Redirect.vue'),
         meta: {}
       }
